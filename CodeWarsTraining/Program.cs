fe07dae7c4dea3db2d114e8f254de8bf6d38f391
SerVass <test@test.com>
2018-05-24 22:35:52 +0300

    Maximum Multiple

--- CodeWarsTraining/Program.cs
+++ CodeWarsTraining/Program.cs
@@ -10,6 +10,18 @@ namespace CodeWarsTraining
     {
         static void Main(string[] args)
         {
+			Console.WriteLine(MaxMultiply(2, 7));
+			Console.WriteLine(MaxMultiply(3, 10));
+			Console.WriteLine(MaxMultiply(7, 17));
+			Console.WriteLine(MaxMultiply(10, 50));
+			Console.WriteLine(MaxMultiply(37, 200));
+			Console.WriteLine(MaxMultiply(7, 100));
+			Console.ReadLine();
+		}
+
+        public static int MaxMultiply(int divisor, int bound)
+        {
+			return bound / divisor * divisor;
         }
     }
 }
