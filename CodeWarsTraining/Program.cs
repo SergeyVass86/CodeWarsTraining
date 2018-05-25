f5cf64b0b557a3f082fccbf2b6e5becc9ac23bb1
SerVass <test@test.com>
2018-05-25 10:32:51 +0300

    altERnaTIng cAsE <=> ALTerNAtiNG CaSe

--- CodeWarsTraining/Program.cs
+++ CodeWarsTraining/Program.cs
@@ -10,48 +10,23 @@ namespace CodeWarsTraining
     {
         static void Main(string[] args)
         {
-			Console.WriteLine(MinimumNumber(new int[] { 3, 1, 2 }));
-			Console.WriteLine(MinimumNumber(new int[] { 5, 2 }));
-			Console.WriteLine(MinimumNumber(new int[] { 1, 1, 1 }));
-			Console.WriteLine(MinimumNumber(new int[] { 2, 12, 8, 4, 6 }));
-			Console.WriteLine(MinimumNumber(new int[] { 50, 39, 49, 6, 17, 28 }));
+			Console.WriteLine("hello world".ToAlternatingCase());
+			Console.WriteLine("HELLO WORLD".ToAlternatingCase());
+			Console.WriteLine("hello WORLD".ToAlternatingCase());
+			Console.WriteLine("HeLLo WoRLD".ToAlternatingCase());
+			Console.WriteLine("12345".ToAlternatingCase());
+			Console.WriteLine("1a2b3c4d5e".ToAlternatingCase());
+			Console.WriteLine("String.ToAlternatingCase".ToAlternatingCase());
+			Console.WriteLine("Hello World".ToAlternatingCase().ToAlternatingCase());
 			Console.ReadLine();
 		}
+	}
 
-		public static int MinimumNumber(int[] numbers)
+	public static class StringExt
+	{
+		public static string ToAlternatingCase(this string s)
 		{
-			int result = 0;
-			var sum = numbers.Sum();
-			bool found = false;
-			while(!found)
-			{
-				if(IsPrime(sum))
-				{
-					found = true;
-				}
-				else
-				{
-					sum++;
-					result++;
-				}
-			}
-			return result;
-		}
-
-		public static bool IsPrime(int num)
-		{
-			if (num == 1) return false;
-			if (num == 2) return true;
-			if (num % 2 == 0) return false;
-
-			var boundary = (int)Math.Floor(Math.Sqrt(num));
-
-			for (int i = 3; i <= boundary; i += 2)
-			{
-				if (num % i == 0) return false;
-			}
-
-			return true;
+			return String.Concat(s.Select(f => char.IsUpper(f) ? char.ToLower(f) : char.IsLower(f) ? char.ToUpper(f) : f));
 		}
 	}
 }
