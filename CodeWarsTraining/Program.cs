fb549fd5b56583dd86a5516da008af18b4fae792
SerVass <test@test.com>
2018-05-25 09:57:36 +0300

    Transform To Prime

--- CodeWarsTraining/Program.cs
+++ CodeWarsTraining/Program.cs
@@ -10,18 +10,48 @@ namespace CodeWarsTraining
     {
         static void Main(string[] args)
         {
-			Console.WriteLine(MaxMultiply(2, 7));
-			Console.WriteLine(MaxMultiply(3, 10));
-			Console.WriteLine(MaxMultiply(7, 17));
-			Console.WriteLine(MaxMultiply(10, 50));
-			Console.WriteLine(MaxMultiply(37, 200));
-			Console.WriteLine(MaxMultiply(7, 100));
+			Console.WriteLine(MinimumNumber(new int[] { 3, 1, 2 }));
+			Console.WriteLine(MinimumNumber(new int[] { 5, 2 }));
+			Console.WriteLine(MinimumNumber(new int[] { 1, 1, 1 }));
+			Console.WriteLine(MinimumNumber(new int[] { 2, 12, 8, 4, 6 }));
+			Console.WriteLine(MinimumNumber(new int[] { 50, 39, 49, 6, 17, 28 }));
 			Console.ReadLine();
 		}
 
-        public static int MaxMultiply(int divisor, int bound)
-        {
-			return bound / divisor * divisor;
-        }
-    }
+		public static int MinimumNumber(int[] numbers)
+		{
+			int result = 0;
+			var sum = numbers.Sum();
+			bool found = false;
+			while(!found)
+			{
+				if(IsPrime(sum))
+				{
+					found = true;
+				}
+				else
+				{
+					sum++;
+					result++;
+				}
+			}
+			return result;
+		}
+
+		public static bool IsPrime(int num)
+		{
+			if (num == 1) return false;
+			if (num == 2) return true;
+			if (num % 2 == 0) return false;
+
+			var boundary = (int)Math.Floor(Math.Sqrt(num));
+
+			for (int i = 3; i <= boundary; i += 2)
+			{
+				if (num % i == 0) return false;
+			}
+
+			return true;
+		}
+	}
 }
