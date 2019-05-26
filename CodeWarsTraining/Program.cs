d7bd052ffb3ce94e2b54e867228d6b3b8171d305
SerVass <test@test.com>
2019-05-26 17:14:15 +0300

    Simple Encryption #4 - Qwerty
    parameters:
    
    1. The string to encrypt/decrypt
    2. The Qwerty-Encryption-Key (000-999)
    The rules are very easy:
    
    The crypting-regions are these 3 lines from your keyboard:
    1. "qwertyuiop"
    2. "asdfghjkl"
    3. "zxcvbnm,."
    
    If a char of the string is not in any of these regions, take the char direct in the output.
    If a char of the string is in one of these regions: Move it by the part of the key in the
    region and take this char at the position from the region.
    If the movement is over the length of the region, continue at the beginning.
    The encrypted char must have the same case like the decrypted char!
    So for upperCase-chars the regions are the same, but with pressed "SHIFT"!
    
    The Encryption-Key is an integer number from 000 to 999. E.g.: 127
    
    The first digit of the key (e.g. 1) is the movement for the first line.
    The second digit of the key (e.g. 2) is the movement for the second line.
    The third digit of the key (e.g. 7) is the movement for the third line.
    
    (Consider that the key is an integer! When you got a 0 this would mean 000. A 1 would mean 001. And so on.)
    You do not need to do any prechecks. The strings will always be not null and will always have a length > 0. You do not have to throw any exceptions.
    
    An Example:
    
    Encrypt "Ball" with key 134
    1. "B" is in the third region line. Move per 4 places in the region. -> ">" (Also "upperCase"!)
    2. "a" is in the second region line. Move per 3 places in the region. -> "f"
    3. "l" is in the second region line. Move per 3 places in the region. -> "d"
    4. "l" is in the second region line. Move per 3 places in the region. -> "d"
    --> Output would be ">fdd"

--- CodeWarsTraining/Program.cs
+++ CodeWarsTraining/Program.cs
@@ -8,26 +8,136 @@ namespace CodeWarsTraining
 {
 	class Program
 	{
-		private const string LOWER = "abcdefghijklmnopqrstuvwxyz";
-		private const string UPPER = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		private const string firstLine = "qwertyuiop";
+		private const string firstLineShift = "QWERTYUIOP";
+		private const string secondLine = "asdfghjkl";
+		private const string secondLineShift = "ASDFGHJKL";
+		private const string thirdLine = "zxcvbnm,.";
+		private const string thirdLineShift = "ZXCVBNM<>";
 
 		static void Main(string[] args)
 		{
-			Console.WriteLine(Rot13("test"));
-			Console.WriteLine(Rot13("Test"));
+			Console.WriteLine(Encrypt("A", 111));
+			Console.WriteLine(Encrypt("Abc", 212));
+			Console.WriteLine(Encrypt("Wave", 0));
+			Console.WriteLine(Encrypt("Wave", 345));
+			Console.WriteLine(Encrypt("Ball", 134));
+			Console.WriteLine(Encrypt("Ball", 444));
+			Console.WriteLine(Encrypt("This is a test.", 348));
+			Console.WriteLine(Encrypt("Do the kata Kobayashi Maru Test. Endless fun and excitement when finding a solution.", 583));
+
+			Console.WriteLine(Decrypt("S", 111));
+			Console.WriteLine(Decrypt("Smb", 212));
+			Console.WriteLine(Decrypt("Wave", 0));
+			Console.WriteLine(Decrypt("Tg.y", 345));
+			Console.WriteLine(Decrypt(">fdd", 134));
+			Console.WriteLine(Decrypt(">gff", 444));
+			Console.WriteLine(Decrypt("Iaqh qh g iyhi,", 348));
+			Console.WriteLine(Decrypt("Sr pgi jlpl Jr,lqlage Zlow Piapc I.skiaa dw. l.s ibnepizi.p ugi. de.se.f l arkwper.c", 583));
 			Console.ReadLine();
 		}
 
-		public static string Rot13(string message)
+		public static string Encrypt(string text, int key)
 		{
-			return String.Join("", message
+			var keyStr = key.ToString("D3").Select(c => c).ToList();
+			return String.Join("", text
 			  .Select(c =>
 			  {
-				  var chars = Char.IsUpper(c) ? UPPER : LOWER;
+				  bool isUpper = Char.IsUpper(c) || thirdLineShift.Contains(c);
+				  int move = 0;
+				  string chars = string.Empty;
+				  if (isUpper)
+				  {
+					  if (firstLineShift.Contains(c))
+					  {
+						  move = int.Parse(keyStr[0].ToString());
+						  chars = firstLineShift;
+					  }
+					  else if (secondLineShift.Contains(c))
+					  {
+						  move = int.Parse(keyStr[1].ToString());
+						  chars = secondLineShift;
+					  }
+					  else if (thirdLineShift.Contains(c))
+					  {
+						  move = int.Parse(keyStr[2].ToString());
+						  chars = thirdLineShift;
+					  }
+				  }
+				  else
+				  {
+					  if (firstLine.Contains(c))
+					  {
+						  move = int.Parse(keyStr[0].ToString());
+						  chars = firstLine;
+					  }
+					  else if (secondLine.Contains(c))
+					  {
+						  move = int.Parse(keyStr[1].ToString());
+						  chars = secondLine;
+					  }
+					  else if (thirdLine.Contains(c))
+					  {
+						  move = int.Parse(keyStr[2].ToString());
+						  chars = thirdLine;
+					  }
+				  }
 				  var idx = chars.IndexOf(c);
 				  return idx == -1 ?
 					c :
-					chars[(idx + 13) % chars.Length];
+					chars[(idx + move) % chars.Length];
+			  }));
+		}
+
+		public static string Decrypt(string encryptedText, int key)
+		{
+			var keyStr = key.ToString("D3").Select(c => c).ToList();
+			return String.Join("", encryptedText
+			  .Select(c =>
+			  {
+				  bool isUpper = Char.IsUpper(c) || thirdLineShift.Contains(c);
+				  int moveBack = 0;
+				  string chars = string.Empty;
+				  if (isUpper)
+				  {
+					  if (firstLineShift.Contains(c))
+					  {
+						  moveBack = int.Parse(keyStr[0].ToString()) * -1;
+						  chars = firstLineShift;
+					  }
+					  else if (secondLineShift.Contains(c))
+					  {
+						  moveBack = int.Parse(keyStr[1].ToString()) * -1;
+						  chars = secondLineShift;
+					  }
+					  else if (thirdLineShift.Contains(c))
+					  {
+						  moveBack = int.Parse(keyStr[2].ToString()) * -1;
+						  chars = thirdLineShift;
+					  }
+				  }
+				  else
+				  {
+					  if (firstLine.Contains(c))
+					  {
+						  moveBack = int.Parse(keyStr[0].ToString()) * -1;
+						  chars = firstLine;
+					  }
+					  else if (secondLine.Contains(c))
+					  {
+						  moveBack = int.Parse(keyStr[1].ToString()) * -1;
+						  chars = secondLine;
+					  }
+					  else if (thirdLine.Contains(c))
+					  {
+						  moveBack = int.Parse(keyStr[2].ToString()) * -1;
+						  chars = thirdLine;
+					  }
+				  }
+				  var idx = chars.IndexOf(c);
+				  return idx == -1 ?
+					c :
+					chars[(idx + moveBack) >= 0 ? idx + moveBack : chars.Length - 1 + (idx + moveBack + 1)];
 			  }));
 		}
 	}
