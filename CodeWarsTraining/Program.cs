c1f5b93262144f4fdb68ce6fe6729757c41d162a
SerVass <test@test.com>
2019-05-26 16:12:48 +0300

    Rot13
    ROT13 is a simple letter substitution cipher that replaces a letter with the letter 13 letters after it in the alphabet. ROT13 is an example of the Caesar cipher.
    
    Create a function that takes a string and returns the string ciphered with Rot13. If there are numbers or special characters included in the string, they should be returned as they are. Only letters from the latin/english alphabet should be shifted, like in the original Rot13 "implementation".

--- CodeWarsTraining/Program.cs
+++ CodeWarsTraining/Program.cs
@@ -10,21 +10,42 @@ namespace CodeWarsTraining
     {
         static void Main(string[] args)
         {
-			int counter = 0;
-			Parallel.For(0, 100, i => counter++);
-			Console.WriteLine(counter);
-			//Console.WriteLine(binaryArrayToNumber(new int[] { 0, 0, 0, 0 }));
-			//Console.WriteLine(binaryArrayToNumber(new int[] { 1, 1, 1, 1 }));
-			//Console.WriteLine(binaryArrayToNumber(new int[] { 0, 1, 1, 0 }));
-			//Console.WriteLine(binaryArrayToNumber(new int[] { 0, 1, 0, 1 }));
-			//Console.WriteLine(binaryArrayToNumber(new int[] { 1, 0, 1, 1 }));
+			Console.WriteLine(Rot13("test"));
+			Console.WriteLine(Rot13("Test"));
 			Console.ReadLine();
 		}
 
-		public static int binaryArrayToNumber(int[] BinaryArray)
+		public static string Rot13(string message)
 		{
-			int i = 0;
-			return BinaryArray.Reverse().Sum(f => (int)(Math.Pow(2, i++)) * f);
+			List<char> charList = message
+				.Select(f => 
+				{
+					if((int)f >= 65 && (int)f <= 90)
+					{
+						int res = (int)f + 13;
+						if(res > 90)
+						{
+							res = 64 + (res - 90);
+						}
+						return (char)res;
+					}
+					else if ((int)f >= 97 && (int)f <= 122)
+					{
+						int res = (int)f + 13;
+						if (res > 122)
+						{
+							res = 96 + (res - 122);
+						}
+						return (char)res;
+					}
+					return f;
+				}).ToList();
+			var sb = new StringBuilder();
+			foreach(var c in charList)
+			{
+				sb.Append(c.ToString());
+			}
+			return sb.ToString();
 		}
 	}
 }
