f2ce6c8ce23baaf476decbd8908d20929620e237
SerVass <test@test.com>
2019-05-27 11:24:44 +0300

    Human Readable Time
    Write a function, which takes a non-negative integer (seconds) as input and returns the time in a human-readable format (HH:MM:SS)
    
    HH = hours, padded to 2 digits, range: 00 - 99
    MM = minutes, padded to 2 digits, range: 00 - 59
    SS = seconds, padded to 2 digits, range: 00 - 59
    The maximum time never exceeds 359999 (99:59:59)
    
    You can find some examples in the test fixtures.
    
    Assert.AreEqual("00:00:00", TimeFormat.GetReadableTime(0));
          Assert.AreEqual("00:00:05", TimeFormat.GetReadableTime(5));
          Assert.AreEqual("00:01:00", TimeFormat.GetReadableTime(60));
          Assert.AreEqual("23:59:59", TimeFormat.GetReadableTime(86399));
          Assert.AreEqual("99:59:59", TimeFormat.GetReadableTime(359999));

--- CodeWarsTraining/Program.cs
+++ CodeWarsTraining/Program.cs
@@ -8,137 +8,26 @@ namespace CodeWarsTraining
 {
 	class Program
 	{
-		private const string firstLine = "qwertyuiop";
-		private const string firstLineShift = "QWERTYUIOP";
-		private const string secondLine = "asdfghjkl";
-		private const string secondLineShift = "ASDFGHJKL";
-		private const string thirdLine = "zxcvbnm,.";
-		private const string thirdLineShift = "ZXCVBNM<>";
-
 		static void Main(string[] args)
 		{
-			Console.WriteLine(Encrypt("A", 111));
-			Console.WriteLine(Encrypt("Abc", 212));
-			Console.WriteLine(Encrypt("Wave", 0));
-			Console.WriteLine(Encrypt("Wave", 345));
-			Console.WriteLine(Encrypt("Ball", 134));
-			Console.WriteLine(Encrypt("Ball", 444));
-			Console.WriteLine(Encrypt("This is a test.", 348));
-			Console.WriteLine(Encrypt("Do the kata Kobayashi Maru Test. Endless fun and excitement when finding a solution.", 583));
-
-			Console.WriteLine(Decrypt("S", 111));
-			Console.WriteLine(Decrypt("Smb", 212));
-			Console.WriteLine(Decrypt("Wave", 0));
-			Console.WriteLine(Decrypt("Tg.y", 345));
-			Console.WriteLine(Decrypt(">fdd", 134));
-			Console.WriteLine(Decrypt(">gff", 444));
-			Console.WriteLine(Decrypt("Iaqh qh g iyhi,", 348));
-			Console.WriteLine(Decrypt("Sr pgi jlpl Jr,lqlage Zlow Piapc I.skiaa dw. l.s ibnepizi.p ugi. de.se.f l arkwper.c", 583));
+			Console.WriteLine(GetReadableTime(0));
+			Console.WriteLine(GetReadableTime(5));
+			Console.WriteLine(GetReadableTime(60));
+			Console.WriteLine(GetReadableTime(86399));
+			Console.WriteLine(GetReadableTime(359999));
 			Console.ReadLine();
 		}
 
-		public static string Encrypt(string text, int key)
+		public static string GetReadableTime(int seconds)
 		{
-			var keyStr = key.ToString("D3").Select(c => c).ToList();
-			return String.Join("", text
-			  .Select(c =>
-			  {
-				  bool isUpper = Char.IsUpper(c) || thirdLineShift.Contains(c);
-				  int move = 0;
-				  string chars = string.Empty;
-				  if (isUpper)
-				  {
-					  if (firstLineShift.Contains(c))
-					  {
-						  move = int.Parse(keyStr[0].ToString());
-						  chars = firstLineShift;
-					  }
-					  else if (secondLineShift.Contains(c))
-					  {
-						  move = int.Parse(keyStr[1].ToString());
-						  chars = secondLineShift;
-					  }
-					  else if (thirdLineShift.Contains(c))
-					  {
-						  move = int.Parse(keyStr[2].ToString());
-						  chars = thirdLineShift;
-					  }
-				  }
-				  else
-				  {
-					  if (firstLine.Contains(c))
-					  {
-						  move = int.Parse(keyStr[0].ToString());
-						  chars = firstLine;
-					  }
-					  else if (secondLine.Contains(c))
-					  {
-						  move = int.Parse(keyStr[1].ToString());
-						  chars = secondLine;
-					  }
-					  else if (thirdLine.Contains(c))
-					  {
-						  move = int.Parse(keyStr[2].ToString());
-						  chars = thirdLine;
-					  }
-				  }
-				  var idx = chars.IndexOf(c);
-				  return idx == -1 ?
-					c :
-					chars[(idx + move) % chars.Length];
-			  }));
-		}
+			if (seconds > 359999) return "";
 
-		public static string Decrypt(string encryptedText, int key)
-		{
-			var keyStr = key.ToString("D3").Select(c => c).ToList();
-			return String.Join("", encryptedText
-			  .Select(c =>
-			  {
-				  bool isUpper = Char.IsUpper(c) || thirdLineShift.Contains(c);
-				  int moveBack = 0;
-				  string chars = string.Empty;
-				  if (isUpper)
-				  {
-					  if (firstLineShift.Contains(c))
-					  {
-						  moveBack = int.Parse(keyStr[0].ToString()) * -1;
-						  chars = firstLineShift;
-					  }
-					  else if (secondLineShift.Contains(c))
-					  {
-						  moveBack = int.Parse(keyStr[1].ToString()) * -1;
-						  chars = secondLineShift;
-					  }
-					  else if (thirdLineShift.Contains(c))
-					  {
-						  moveBack = int.Parse(keyStr[2].ToString()) * -1;
-						  chars = thirdLineShift;
-					  }
-				  }
-				  else
-				  {
-					  if (firstLine.Contains(c))
-					  {
-						  moveBack = int.Parse(keyStr[0].ToString()) * -1;
-						  chars = firstLine;
-					  }
-					  else if (secondLine.Contains(c))
-					  {
-						  moveBack = int.Parse(keyStr[1].ToString()) * -1;
-						  chars = secondLine;
-					  }
-					  else if (thirdLine.Contains(c))
-					  {
-						  moveBack = int.Parse(keyStr[2].ToString()) * -1;
-						  chars = thirdLine;
-					  }
-				  }
-				  var idx = chars.IndexOf(c);
-				  return idx == -1 ?
-					c :
-					chars[(idx + moveBack) >= 0 ? idx + moveBack : chars.Length - 1 + (idx + moveBack + 1)];
-			  }));
+			TimeSpan t = TimeSpan.FromSeconds(seconds);
+
+			return string.Format("{0:D2}:{1:D2}:{2:D2}",
+							t.Hours + (t.Days > 0 ? t.Days * 24 : 0),
+							t.Minutes,
+							t.Seconds);
 		}
 	}
 }
